75ad5563a068f6f3d18596f97f9b57c00fc3b8f1
magentapenguin <83044109+magentapenguin@users.noreply.github.com>
2024-10-22 21:07:14 +0000

    Refactor bookmarklet code to remove scrollbar

--- bookmarklet-uncompiled.js
+++ bookmarklet-uncompiled.js
@@ -4,7 +4,7 @@
 
 (() => { // start bookmarklet
     const newwindow = window.open('about:blank', 'bookmarklet');
-    newwindow.document.write(`<!DOCTYPE html><html style="width:100%;height:100%;border:none;margin:0;overflow:none;"><head><title>about:blank</title></head><body style="width:100%;height:100%;border:none;margin:0;overflow:none;"><iframe src="${location.href}" style="width:100%;height:100%;border:none;margin:0;overflow:none;"></iframe></body></html>`);
+    newwindow.document.write(`<!DOCTYPE html><html style="width:100%;height:100%;border:none;margin:0;overflow:hidden;"><head></head><body style="width:100%;height:100%;border:none;margin:0;overflow:hidden;"><iframe src="${location.href}" style="width:100%;height:100%;border:none;margin:0;overflow:hidden;"></iframe></body></html>`);
     setTimeout(close.bind(window), 500);
     newwindow.focus();
 })(); // end bookmarklet
