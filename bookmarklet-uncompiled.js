08b94d8052f2fe56f7e48d9e97d864892224a8e0
magentapenguin <83044109+magentapenguin@users.noreply.github.com>
2024-10-23 18:13:40 +0000

    stuff

--- bookmarklet-uncompiled.js
+++ bookmarklet-uncompiled.js
@@ -3,8 +3,8 @@
 // not minified
 
 (() => { // start bookmarklet
-    const newwindow = window.open('about:blank', 'bookmarklet');
-    newwindow.document.write(`<!DOCTYPE html><html style="width:100%;height:100%;border:none;margin:0;overflow:hidden;"><head></head><body style="width:100%;height:100%;border:none;margin:0;overflow:hidden;"><iframe src="${location.href}" style="width:100%;height:100%;border:none;margin:0;overflow:hidden;"></iframe></body></html>`);
+    const newwindow = window.open('about:blank');
+    newwindow.document.write(`<!DOCTYPE html><html style="width:100%;height:100%;border:none;margin:0;overflow:hidden"><head><style>body,html,iframe{box-sizing:border-box;width:100%;height:100%;border:none;margin:0;overflow:hidden}</style></head><body><div id="site"></div><script>const site=document.getElementById("site"),shadow=site.attachShadow({mode:"closed"});fetch("${location.href}",{mode:"no-cors"}).then((e=>e.text())).then((e=>{shadow.innerHTML=e}));</script></body></html>`);
     setTimeout(close.bind(window), 500);
     newwindow.focus();
 })(); // end bookmarklet
